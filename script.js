7a534dfbd6b66090a144000ac6e770b4481e1410
hediyeh <hediyehknm@gmail.com>
2025-08-17 00:08:56 +0330

    put and delete

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const users = require("./user");
+const { body, validationResult } = require("express-validator");
+let users = require("./user");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
@@ -25,7 +26,78 @@ app.get("/user/na/h/:id", (req, res) => {
     res.json({ data: req.params.id, mes: 200 });
 });
 
-app.post("/user/na/h/post", (req, res) => {
-    users.push({ id: users.length + 1, ...req.body });
-    res.json({ data: users, mes: 200 });
-});
+app.post(
+    "/user/na/h/post",
+
+    [
+        body("n", "n must valid").notEmpty(),
+        body("e", "e must be valid").isEmail(),
+    ],
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ data: null, errors: errors.array(), mes: "VE" });
+        }
+
+        users.push({ id: users.length + 1, ...req.body });
+        res.json({ data: users, mes: 200 });
+    }
+);
+
+app.put(
+    "/user/na/h/put/:id",
+
+    [
+        body("n", "n must valid").notEmpty(),
+        body("e", "e must be valid").isEmail(),
+    ],
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ data: null, errors: errors.array(), msg: "validation error" });
+        }
+
+        const userIndex = users.findIndex((u) => u.id == req.params.id);
+        if (userIndex === -1) {
+            return res.status(404).json({ data: null, errors: [], msg: "not found" });
+        }
+
+        users[userIndex] = {...users[userIndex], ...req.body };
+
+        res.json({
+            data: users,
+            msg: "ok",
+        });
+    }
+);
+
+app.delete(
+    "/user/na/h/delete/:id",
+
+    [body("n", "n valid").notEmpty(), body("e", "e valid").isEmail()],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ data: null, errors: errors.array(), msg: "validation error" });
+        }
+
+        const userIndex = users.findIndex((u) => u.id == req.params.id);
+        if (userIndex === -1) {
+            return res.status(404).json({ data: null, errors: [], msg: "not found" });
+        }
+
+        users.splice(userIndex, 1);
+        res.json({
+            data: users,
+            msg: "ok",
+        });
+    }
+);
